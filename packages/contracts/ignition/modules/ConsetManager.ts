b9737805ec79076e675af22eb849dde57f0ab99a
alessandromodelli <alessandro.modelli2@studio.unibo.it>
2026-04-21 09:46:18 +0200

    removed hardhat examples

--- packages/contracts/ignition/modules/ConsetManager.ts
+++
@@ -1,8 +0,0 @@
-import { buildModule } from "@nomicfoundation/hardhat-ignition/modules";
-
-const ConsentManagerModule = buildModule("ConsentManagerModule", (m) => {
-  const consentManager = m.contract("ConsentManager");
-  return { consentManager };
-});
-
-export default ConsentManagerModule;
